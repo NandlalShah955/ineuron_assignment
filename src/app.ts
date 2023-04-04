8475d708c5017a8453ced2d517e604f8d26bd8d1
Nandlal <shahnandlal49@gmail.com>
2023-04-04 20:40:46 +0530

    working on typescript

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,32 @@
 
-// import express from "express";
-// import db from "mongoose";
-// import todoRoutes from "./routes/task";
-// import { json, urlencoded } from "body-parser";
+import express from "express";
+import db from "mongoose";
+import todoRoutes from "./routes/task";
+import { json, urlencoded } from "body-parser";
 
-// const app = express();
-// const PORT=8080;
-// app.use(json());
-// app.use(urlencoded({ extended: true }));
-// app.use("/task", todoRoutes);
-// app.use(
-//   (
-//     err: Error,
-//     req: express.Request,
-//     res: express.Response,
-//     next: express.NextFunction
-//   ) => {
-//     res.status(500).json({ message: err.message });
-//   }
-// );
-// db.connect("mongodb://localhost:27017/backend",()=>{
+const app = express();
+const PORT=8080;
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use("/task", todoRoutes);
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    res.status(500).json({ message: err.message });
+  }
+);
+db.connect("mongodb://localhost:27017/backend",()=>{
 
-//     console.log("Database connected");
-// });
-// app.listen(PORT,async()=>{
-//     try {
-//      console.log(`listening on ${PORT}`);
-//     } catch (error) {
-//         console.log(error)
-//     }
-// });
+    console.log("Database connected");
+});
+app.listen(PORT,async()=>{
+    try {
+     console.log(`listening on ${PORT}`);
+    } catch (error) {
+        console.log(error)
+    }
+});
